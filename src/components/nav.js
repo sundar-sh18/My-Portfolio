6dc421c2edf6f9b90e8aad11efc647e12106c3cb
maaxone098 <sundar.sh18@gmail.com>
2025-07-27 21:42:00 +0530

    updated nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,16 +1,16 @@
 // import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedinIn, faWhatsapp, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faImage } from '@fortawesome/free-solid-svg-icons';
 import './components.css';
-
 export default function Nav(){
 
 
 
 
     return(
-        <nav>
+     
+     
+          <nav>
             <div className="logo">
                <a href='/' >
                     <FontAwesomeIcon icon={faImage} />
@@ -23,23 +23,8 @@ export default function Nav(){
                <a href='#skills' >skills</a>
                <a href='#contact' >contact</a>
             </div>
-            <div className="icons"> 
-               <a href='https://github.com/sundar-sh18' target='_blank' rel="noreferrer noopener">
-                    <FontAwesomeIcon icon={faGithub} />
-               </a>
-                
-               <a href='https://www.linkedin.com/in/sundar-sh18/' target='_blank' rel="noreferrer noopener">
-                    <FontAwesomeIcon icon={faLinkedinIn} />
-               </a>
-               
-               <a href='https://wa.me/9964255131' target='_blank' rel="noreferrer noopener">
-                    <FontAwesomeIcon icon={faWhatsapp} />
-               </a>
-               <a href='https://www.instagram.com/_s.u.n.d.a.r._.s_/' target='_blank' rel="noreferrer noopener">
-                    <FontAwesomeIcon icon={faInstagram} />
-               </a>
-            
-            </div>
         </nav>
+    
+        
     )
 }
