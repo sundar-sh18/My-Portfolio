d3d9785733d512032bc645eb3f144a54c9a7cbd3
maaxone098 <sundar.sh18@gmail.com>
2025-07-27 21:40:23 +0530

    home animation

--- src/components/home.js
+++ src/components/home.js
@@ -1,35 +1,60 @@
 import React from 'react';
 import './components.css';
-import Img from '../assets/Sundar_pic.JPG'
+// import Img from '../assets/Sundar_pic.JPG';
+import SplitText from '../reactBits/TextAnimations/SplitText/SplitText';
+import Lightning from '../reactBits/Backgrounds/Lightning/Lightning';
+import Nav from './nav';
+
 
 export default function Home(){
 
 
 
     return(
-        <>
-           <div id='/' className='Home'>
-                <div className='Sundar'>
-                    <img src={Img} alt='Sundar S'/>
-                </div>
-                <div className='intro'> 
-                    <h3>hi, i am sundar.</h3>
-                    <h1>A passionate developer based in Bangalore</h1>
-                    <p>Web developer crafting responsive, user-friendly websites.
-                        Focused on clean code, performance, and great user experience.</p>
-                    <div className="buttons">
-                        <button>Got a project?</button>
-                        <button>lets talk!</button>
-                    </div>
-                </div>
-           </div>
-         
-                <div className="marquee">
-                    <div className="marquee-content">
-                        <h3>This is a modern marquee effect using CSS animations!</h3> 
-                    </div>
-                </div>
+    <>
     
-        </>
+        
+               <div style={{ width: '100%', height: '100vh', position: 'relative' }} className='spark'>
+              <Lightning
+                hue={260}
+                xOffset={-0.7}
+                speed={1}
+                intensity={1}
+                size={1}
+                />
+                <div id='/' className='Home'>
+                    <Nav/>
+                    {/* <div className='Sundar'>
+                        <img src={Img} alt='Sundar S'/>
+                    </div> */}
+                    <div className='intro'> 
+                    <SplitText
+                            text="Hi, I Am Sundar."
+                            className="text-2xl font-semibold text-center h3"
+                            delay={100}
+                            duration={0.6}
+                            ease="power3.out"
+                            splitType="chars"
+                            from={{ opacity: 0, y: 40 }}
+                            to={{ opacity: 1, y: 0 }}
+                            threshold={0.1}
+                            rootMargin="-100px"
+                            textAlign="center"
+                        />
+                        <h1>A passionate developer based in Bangalore</h1>
+                        <p>Web developer crafting responsive, user-friendly websites.
+                            Focused on clean code, performance, and great user experience.</p>
+                        <div className="buttons">
+                            <button>Got a project?</button>
+                            <button>lets talk!</button>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            
+           
+           
+    </>
     )
 }
