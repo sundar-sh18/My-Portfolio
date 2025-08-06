a69adc2311d341d2eb081dc1c1a19d582f1767a4
maaxone098 <sundar.sh18@gmail.com>
2025-08-06 13:57:51 +0530

    lightining background

--- src/App.js
+++ src/App.js
@@ -3,18 +3,31 @@ import './App.css';
 import Home from './components/home';
 import Projects from './components/projects';
 import ClickSpark from './reactBits/Animations/ClickSpark/ClickSpark';
+import Lightning from './reactBits/Backgrounds/Lightning/Lightning';
+
+
 
 function App() {
   return (
     <>
     <ClickSpark
-                sparkColor='#8750ea'
+                sparkColor='white'
                 sparkSize={15}
                 sparkRadius={55}
                 sparkCount={9}
                 duration={900}
               > 
-      <Home/>
+      <div style={{ width: '100%', height: '100vh', position: 'relative' }} className='spark'>
+        <Lightning
+          hue={260}
+          xOffset={-0.7}
+          speed={1}
+          intensity={1}
+          size={1}
+          />
+          
+          <Home/>
+      </div>
       <Projects/>
       <div style={{ width: '100%', height: '100vh', position: 'relative' }}></div>
       </ClickSpark>
