90bd8ecb17fddc5bb6b0b28334dd6314d15b1914
maaxone098 <sundar.sh18@gmail.com>
2025-08-13 18:48:23 +0530

    nav bar

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,29 +1,26 @@
-// import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCode } from '@fortawesome/free-solid-svg-icons';
-import './components.css';
-export default function Nav(){
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCode, faBars} from "@fortawesome/free-solid-svg-icons";
+import "./components.css";
 
+export default function Nav() {
 
-
-
-    return(
+  return (
+    <nav className="navbar">
+      <a href="/" className="nav-logo">
+        <FontAwesomeIcon icon={faCode} />
+      </a>
+      
+        <div className="menu-icon">
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+        <div className="navi desktop">
+          <a href="#home">Home</a>
+          <a href="#projects">Projects</a>
+          <a href="#resume">Resume</a>
+          <a href="#skills">Skills</a>
+          <a href="#contact">Contact</a>
+        </div>
      
-     
-          <nav>
-               <a href='/' className='nav-logo' >
-                    <FontAwesomeIcon icon={faCode} />
-               </a>
-            
-            <div className="navi">
-               <a href='#home' >home</a>
-               <a href='#projects' >projects</a>
-               <a href='#resume' >resume</a>
-               <a href='#skills' >skills</a>
-               <a href='#contact' >contact</a>
-            </div>
-        </nav>
-    
-        
-    )
+    </nav>
+  );
 }
